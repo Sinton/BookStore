feef47256480e1301c3982da92d9353c9a2a9aec
Sinton <yansuntong@gmail.com>
2016-07-03 02:32:48 +0800

    添加对字符串的MD5加密

--- src/edu/zjut/utils/Helper.java
+++ src/edu/zjut/utils/Helper.java
@@ -1,5 +1,7 @@
 package edu.zjut.utils;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -40,5 +42,41 @@ public class Helper {
 		}
 		String time = timeFormat.format(new Date(timeStamp * 1000));
 		return time;
+	}
+
+	/**
+	 * 将字符串进行Hash加密
+	 * @param source 需要加密的字符串
+	 * @param hashType 加密类型（MD5 和 SHA）
+	 * @return
+	 */
+	public String getHash(String source, String hashType) {
+		// 用来将字节转换成 16 进制表示的字符
+		char hexDigits[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+		try {
+			MessageDigest md = MessageDigest.getInstance(hashType);
+			// 通过使用 update 方法处理数据,使指定的 byte数组更新摘要
+			md.update(source.getBytes());
+			// 获得密文完成哈希计算,产生128 位的长整数
+			byte[] encryptStr = md.digest();
+			// 每个字节用 16 进制表示的话，使用两个字符
+			char str[] = new char[16 * 2];
+			// 表示转换结果中对应的字符位置
+			int k = 0;
+			// 从第一个字节开始，对每一个字节,转换成 16 进制字符的转换
+			for (int i = 0; i < 16; i++) {
+				// 取第 i 个字节
+				byte byte0 = encryptStr[i];
+				// 取字节中高 4 位的数字转换, >>>
+				str[k++] = hexDigits[byte0 >>> 4 & 0xf];
+				// 取字节中低 4 位的数字转换
+				str[k++] = hexDigits[byte0 & 0xf];
+			}
+			// 换后的结果转换为字符串
+			return new String(str);
+		} catch (NoSuchAlgorithmException exception) {
+			exception.printStackTrace();
+		}
+		return null;
 	}
 }
